9b9fd6539f0d13d0ff823a966f51f657da81973e
Platosdog <62915572+Platosdog@users.noreply.github.com>
2021-04-01 15:38:20 -0500

    step5

---
+++ SimpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        public string GetGreeting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
