91d78b774c3e7d978b42e736e39d3f8d260fd0da
Platosdog <62915572+Platosdog@users.noreply.github.com>
2021-04-01 15:48:14 -0500

    step5

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace SimpleCrm.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
