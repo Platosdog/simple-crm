ac577d766080946d2c4a8e9645f6d6c4db6dba01
Platosdog <62915572+Platosdog@users.noreply.github.com>
2021-04-13 19:16:22 -0500

    missing controller

---
+++ SimpleCrm/SimpleCrm.Web/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCrm.Web.Controllers
+{
+    public class ContactController
+    {
+        public string List(string phone) 
+        {
+            return phone;
+        }
+    }
+}
