9ecc6b1ccd6742f7a6306562cefc34274a4e5ee8
Platosdog <62915572+Platosdog@users.noreply.github.com>
2021-05-06 19:26:40 -0500

    Step 14.1

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -20,7 +20,7 @@ namespace SimpleCrm.Web
         {
             services.AddMvc();
             services.AddSingleton<IGreeter, ConfigurationGreeter>();
-            services.AddSingleton<InMemoryCustomerData>();
+            services.AddSingleton<ICustomerData, InMemoryCustomerData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
