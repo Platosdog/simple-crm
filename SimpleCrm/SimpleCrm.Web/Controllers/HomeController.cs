aeedda34223482bdb8c939d45ed59984409ba139
Platosdog <62915572+Platosdog@users.noreply.github.com>
2021-07-05 22:11:53 -0500

    step16

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -4,20 +4,30 @@ namespace SimpleCrm.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private ICustomerData customerData;
-        private readonly IGreeter greeter;
+        private readonly ICustomerData customerData;
+        private readonly IGreeter _greeter;
 
-        public HomeController(ICustomerData customerData, IGreeter greeter)
+        public HomeController(ICustomerData customerData, IGreeter greeter) 
         {
             this.customerData = customerData;
-            this.greeter = greeter;
+            _greeter = greeter;
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = customerData.Get(id);
+            if (customer == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
         }
 
         public IActionResult Index()
         {
              var model = new HomePageViewModel()
             {
-                CurrentMessage = greeter.GetGreeting(),
+                CurrentMessage = _greeter.GetGreeting(),
                 Customers = customerData.GetAll()
             };
             return View(model);
--- SimpleCrm/SimpleCrm/InMemoryCustomerData.cs
+++ SimpleCrm/SimpleCrm/InMemoryCustomerData.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 namespace SimpleCrm
 {
     public class InMemoryCustomerData : ICustomerData
     {
-        IList<Customer> _customers; 
+        IList<Customer> customers;
 
         public InMemoryCustomerData()
-    {
-        _customers = new List<Customer>
+        {
+            customers = new List<Customer>
                   {
                       new Customer { Id =1, FirstName ="Bob", LastName = "Jones", PhoneNumber = "555-555-2345" },
                       new Customer { Id =2, FirstName ="Jane", LastName = "Smith", PhoneNumber = "555-555-5256" },
@@ -18,16 +19,19 @@ namespace SimpleCrm
                       new Customer { Id =5, FirstName ="James", LastName = "Dean", PhoneNumber = "555-555-7245" },
                       new Customer { Id =6, FirstName ="Michelle", LastName = "Leary", PhoneNumber = "555-555-3457" }
                   };
-    }
+        }
 
         public Customer Get(int id)
         {
-            throw new NotImplementedException();
+            return customers.FirstOrDefault(cust =>
+            {
+                return cust.Id == id;
+            });
         }
 
         public IEnumerable<Customer> GetAll()
-    {
-        return _customers;
+        {
+            return customers;
+        }
     }
-}
 }
