39fadfbdf1b671f55d8017e6f49b04b0921b1a54
Platosdog <62915572+Platosdog@users.noreply.github.com>
2022-02-01 18:42:36 -0600

    step 35

--- SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.Web.Models.Account;
 
 namespace SimpleCrm.Web.Controllers
 {
@@ -11,6 +13,38 @@ namespace SimpleCrm.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+        private readonly UserManager<CrmUser> userManager;
+        public AccountController(UserManager<CrmUser> userManager)
+        {  
+            this.userManager = userManager;
+        }
+        private readonly SignInManager<CrmUser> signInManager;
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new CrmUser
+                {
+                    UserName = model.UserName,
+                    DisplayName = model.DisplayName,
+                    Email = model.UserName
+                };
+                var createResult = await this.userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await this.signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var result in createResult.Errors)
+                {
+                    ModelState.AddModelError("", result.Description);
+                }
+                return NoContent(); 
+            }
             return View();
         }
     }
