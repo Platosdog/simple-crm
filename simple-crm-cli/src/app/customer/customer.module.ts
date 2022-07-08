0a038f76106dc49cdcfd2e83c3b298b82fe12b7c
Platosdog <62915572+Platosdog@users.noreply.github.com>
2022-07-07 19:20:45 -0500

    58a: adding flex import

--- simple-crm-cli/src/app/customer/customer.module.ts
+++ simple-crm-cli/src/app/customer/customer.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 
-import {MatTableModule} from '@angular/material/table';
-import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { CustomerService } from './customer.service';
 import { Observable } from 'rxjs';
 import { Customer } from './customer.model';
 import { CustomerMockService } from './customer-mock.service';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 
 @NgModule({
@@ -23,6 +27,9 @@ import { CustomerMockService } from './customer-mock.service';
     CustomerRoutingModule,
     MatTableModule,
     MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [
     {
