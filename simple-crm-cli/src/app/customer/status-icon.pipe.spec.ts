3ce1d730f6826849fb44ac5b0264d08bb38caf83
Platosdog <62915572+Platosdog@users.noreply.github.com>
2022-10-26 08:34:49 +0900

    66: it works!

--- simple-crm-cli/src/app/customer/status-icon.pipe.spec.ts
+++ simple-crm-cli/src/app/customer/status-icon.pipe.spec.ts
@@ -6,3 +6,8 @@ describe('StatusIconPipe', () => {
     expect(pipe).toBeTruthy();
   });
 });
+it('Prospect should result in online', () => {
+  const pipe = new StatusIconPipe(); // 1. SETUP: construct a new instance of the class.
+  const x = pipe.transform('Prospect'); // 2. INVOKE the method
+  expect(x).toEqual('online'); // 3. VERIFY the result of the method matches what is expected.
+});
