8e96816aef12ff3c6b4df42b0242d08cf79941f7
Platosdog <62915572+Platosdog@users.noreply.github.com>
2022-10-28 23:38:15 +0900

    68: unit testing app component

--- simple-crm-cli/src/app/app.component.spec.ts
+++ simple-crm-cli/src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatLine, MatRipple } from '@angular/material/core';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatList, MatListIconCssMatStyler, MatListModule, MatNavList } from '@angular/material/list';
+import { MatSidenav, MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterLinkActive } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +34,30 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simple-crm-cli'`, () => {
+  it(`should have as title 'Simple Crm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simple-crm-cli');
+    expect(app.title).toEqual('Simple Crm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-crm-cli app is running!');
+    expect(compiled.querySelector('span.title')?.textContent).toContain('Simple Crm');
+  });
+
+  it('should have menu item - customers', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.mat-line')?.textContent).toContain('Customers');
+  });
+
+  it('should have menu item - Pipeline', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.chatlist .mat-line')?.textContent).toContain('Pipeline');
   });
 });
