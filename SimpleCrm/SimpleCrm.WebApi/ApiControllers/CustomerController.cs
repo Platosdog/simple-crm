e8ecb06a4b921102162d0a8e4b9f3b374b66b702
Platosdog <62915572+Platosdog@users.noreply.github.com>
2022-12-12 21:29:52 +0900

    105a: create controller with injected dependencies

---
+++ SimpleCrm/SimpleCrm.WebApi/ApiControllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace SimpleCrm.WebApi.ApiControllers
+{
+    [Route("api/customer")]
+    public class CustomerController : Controller
+    {
+        [HttpGet("")] 
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")] 
+        public IActionResult Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("")] 
+        public IActionResult Create([FromBody] Customer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")] 
+        public IActionResult Update(int id, [FromBody] Customer model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")] 
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
