cf6aa4e8a9b3dc518ca81979afb915ff66cfba4d
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-04-06 22:42:19 +0900

    117: Authenticate name/password with ASP.Net Identity

--- SimpleCrm.WebApi/ApiControllers/AuthController.js
+++ SimpleCrm.WebApi/ApiControllers/AuthController.js
@@ -4,6 +4,15 @@ using System.Threading.Tasks;
 namespace SimpleCrm.WebApi.ApiControllers {
     public class AuthController : Controller
     {
+            private readonly UserManager < CrmUser > _userManager;
+    private readonly JwtFactory _jwtFactory;
+
+    public AuthController(UserManager < CrmUser > userManager, JwtFactory jwtFactory)
+        {
+            this._userManager = userManager;
+            this._jwtFactory = jwtFactory;
+        }
+
         [HttpPost("login")]
           public async Task < IActionResult > Post([FromBody] CredentialsViewModel credentials)
         { // TODO: create a CredentialsViewModel class in the next assignment
@@ -21,6 +30,45 @@ namespace SimpleCrm.WebApi.ApiControllers {
             var userModel = await GetUserData(user);
             // returns a UserSummaryViewModel containing a JWT and other user info
             return Ok(userModel);
+        }
+        
+    private async Task < CrmUser > Authenticate(string emailAddress, string password)
+        {
+            if (emailAddress == "" || password == "") {
+                return null;
+            }
+
+            var currentUser = await _userManager.FindByEmailAsync(emailAddress);
+            if (currentUser == null) {
+                var rand = new Random(DateTime.Now.Second).Next(2, 38);
+                await Task.Delay(rand);
+                return null;
+            }
+
+            var isUser = await _userManager.CheckPasswordAsync(currentUser, password);
+            if (isUser) {
+                return currentUser;
+            }
+
+            return null;
+        }
+
+    private async Task < UserSummaryViewModel > GetUserData(CrmUser user)
+        {
+            if (user == null) return null;
+
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Count == 0) {
+                roles.Add("prospect");
+            }
+
+            var jwt = await _jwtFactory.GenerateEncodedToken(user.UserName, _jwtFactory.GenerateClaimsIdentity(user.UserName, user.Id.ToString()));
+            var userModel = new UserSummaryViewModel
+            {
+                Id = user.Id,
+                    Name = user.UserName,
+                    emailAddress = user.Email
+            };
         }
     }
 }
---
+++ SimpleCrm.WebApi/Models/UserSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SimpleCrm.WebApi.Models
+{
+    public class UserSummaryViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string JWT { get; set; }
+        public ArraySegment<String> Roles { get; set; }
+        public string AccountID { get; set; }
+    }
+}
