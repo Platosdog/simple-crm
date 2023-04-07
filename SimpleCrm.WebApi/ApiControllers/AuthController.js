422d7a02bcf786d37ad11791115b23d9a1dec9e6
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-04-07 09:01:39 +0900

    118: get user data with JWT for authenticated user

--- SimpleCrm.WebApi/ApiControllers/AuthController.js
+++ SimpleCrm.WebApi/ApiControllers/AuthController.js
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SimpleCrm.WebApi.ApiControllers {
     public class AuthController : Controller
@@ -15,20 +16,20 @@ namespace SimpleCrm.WebApi.ApiControllers {
 
         [HttpPost("login")]
           public async Task < IActionResult > Post([FromBody] CredentialsViewModel credentials)
-        { // TODO: create a CredentialsViewModel class in the next assignment
+        { 
             if (!ModelState.IsValid) {
                 return UnprocessableEntity(ModelState);
             }
 
-            // TODO: add Authenticate method (see lesson below)
+            
             var user = await Authenticate(credentials.EmailAddress, credentials.Password);
             if (user == null) {
                 return UnprocessableEntity("Invalid username or password.");
             }
 
-            // TODO: add GetUserData method (see lesson below)
+            
             var userModel = await GetUserData(user);
-            // returns a UserSummaryViewModel containing a JWT and other user info
+            
             return Ok(userModel);
         }
         
